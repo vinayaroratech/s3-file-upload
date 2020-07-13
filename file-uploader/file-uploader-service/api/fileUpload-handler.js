e545e1b45ce230f55fa98687433ab47a03e1883c
Vinay Arora <vinay.arora7171@hotmail.com>
2020-07-13 22:00:22 +0530

    Added list endpoint

--- file-uploader/file-uploader-service/api/fileUpload-handler.js
+++ file-uploader/file-uploader-service/api/fileUpload-handler.js
@@ -63,3 +63,31 @@ const fileUploadInfo = (fullname, email, experience) => {
     updatedAt: timestamp,
   };
 };
+
+module.exports.list = (event, context, callback) => {
+  var params = {
+    TableName: process.env.FILE_UPLOAD_TABLE,
+    ProjectionExpression: "id, fullname, email, experience, submittedAt, updatedAt"
+  };
+
+  console.log("Scanning file upload table.");
+  const onScan = (err, data) => {
+
+    if (err) {
+      console.log('Scan failed to load data. Error JSON:', JSON.stringify(err, null, 2));
+      callback(err);
+    } else {
+      console.log("Scan succeeded.");
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          files: data.Items
+        })
+      });
+    }
+
+  };
+
+  dynamoDb.scan(params, onScan);
+
+};
